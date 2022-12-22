ba8c4e82e866a9b8cdd53a93e71adc3642fea87c
Steven Enamakel <enamakel@mahadao.com>
2022-12-23 03:47:47 +0530

    update strategy

--- src/views/arth-eth/components/StrategyInfo.tsx
+++ src/views/arth-eth/components/StrategyInfo.tsx
@@ -16,8 +16,8 @@ const StrategyInfo = () => {
           <p className={'m-b-8'}>The ETH staking program is a simple way for users to earn a yield on their
             ETH by providing liquidity to the MahaDAO ecosystem.</p>
           <p className={'m-b-8'}>The ETH you'll contribute here is used as collateral to mint ARTH (an
-            inflation-proof stablecoin) and then deposited into MahaLend to provide liquidity for the ecosystem.</p>
-          <p className={'m-b-8'}>Rewards are given out in MAHA, the governance token. You can further lock MAHA for more staking rewards and other special benefits
+            inflation-proof stablecoin) and then it is deposited into MahaLend to provide liquidity for the ecosystem.</p>
+          <p className={'m-b-8'}>Rewards are given out in MAHA, the governance token for the entire ecosystem. You can further lock MAHA for more staking rewards and other special benefits
             from the <a href={'https://gov.mahadao.com/#/locker'} className={'links'}>governance portal</a>.</p>
           <p className={'m-b-8'}>
             You can withdraw your ETH at any time from the program.
